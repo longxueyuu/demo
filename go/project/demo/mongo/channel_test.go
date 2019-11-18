9d8cb832b612ff1194f840e85c906c3cff164653
longxueyuu <longxueyuu@gmail.com>
2019-11-18 11:51:03 +0800

    ADD: .gitignore

---
+++ go/project/demo/mongo/channel_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNilChannel(t *testing.T) {
+	var ch chan int
+	fmt.Println(ch)
+	v, ok := <-ch
+	fmt.Println(v, ok)
+}
+
+func TestCloseChannel(t *testing.T) {
+	var ch = make(chan int)
+	fmt.Println(ch)
+
+	close(ch)
+
+	v, ok := <-ch
+	fmt.Println(v, ok)
+}
+
+func TestForRangeChannel(t *testing.T) {
+	var ch = make(chan int, 10)
+
+	ch<- 1
+	ch<- 2
+
+	fmt.Println("start")
+	for v := range ch {
+		fmt.Println(v)
+	}
+	fmt.Println("end")
+}
