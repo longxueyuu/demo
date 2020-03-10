0217edb6ce83a0ee7567130ef997b26114785e4b
longxueyuu <longxueyuu@gmail.com>
2020-03-10 14:07:09 +0800

    MOD: refactor

--- go/project/demo/HelloWorld.go
+++ go/project/demo/HelloWorld.go
@@ -1,28 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"path"
+	"runtime"
 	"time"
 )
 
 func main() {
-	fmt.Printf("hello, world\nhello you\n")
+	log.Printf("hello, world")
 
 	t, _ := time.ParseInLocation("2006-01-02 15:04:05", "2016-06-13 15:34:39", time.Local)
 	// 整点（向下取整）
-	fmt.Println(t.Truncate(1 * time.Hour))
+	log.Println(t.Truncate(1 * time.Hour))
 	// 整点（最接近）
-	fmt.Println(t.Round(1 * time.Hour))
+	log.Println(t.Round(1 * time.Hour))
 
 	// 整分（向下取整）
-	fmt.Println(t.Truncate(1 * time.Minute))
+	log.Println(t.Truncate(1 * time.Minute))
 	// 整分（最接近）
-	fmt.Println(t.Round(1 * time.Minute))
+	log.Println(t.Round(1 * time.Minute))
 
 	t2, _ := time.ParseInLocation("2006-01-02 15:04:05", t.Format("2006-01-02 15:00:00"), time.Local)
-	fmt.Println(t2)
+	log.Println(t2)
 
 	dir, file := path.Split("")
-	fmt.Printf("dir=%s, file=%s", dir, file)
+	log.Printf("dir=%s, file=%s", dir, file)
+
+	log.Printf("NumCPU=%v", runtime.NumCPU())
 }
